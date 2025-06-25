5eee885a408090f4f7b372ea08a5083a3b93de98
Cid Oliveira <alcides.angelo10@gmail.com>
2025-06-25 17:10:12 -0300

    quiz css & js initial logic - #17

---
+++ js/quiz.js
@@ -0,0 +1,79 @@
+let bachelors = {
+  alex: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  elliott: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  harvey: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  sam: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  sebastian: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  shane: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  abigail: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  haley: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  leah: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  maru: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  penny: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+  emily: {
+    counter: 0,
+    message: "",
+    avatar: "",
+  },
+};
+
+let quizContent = {
+  question1: {
+    question: "1. It’s a totally free Saturday. You’d rather…",
+    option1: "Dive into the mines or beat a rogue-like high score.",
+    option2: "Hunt the cutest coffee spot for an photo shoot.",
+    option3: "Jam with the band and grab a burger on the way home.",
+    option4: "Hit the field for a serious workout, then protein shake.",
+  },
+  question2: {
+    question: "2. Rainy-afternoon bliss looks like …",
+    option1: "Reading stories to kids in the library corner.",
+    option2: "Sculpting or painting while it drizzles outside.",
+    option3: "Drafting the next chapter of your novel by the sea.",
+    option4: "Sewing a wild outfit and dancing in the living-room.",
+  },
+};
